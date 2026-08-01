1707b4c64baa30e538a8fef33798e0ff3bc710a2
rizoshka12210 <rizoshka12210@gmail.com>
2026-08-01 09:12:47 +0500

    Add gitignore and AppDbContext

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3f28543585a4bde5cb94286ec18fd8b646e8cad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32ec5e0d286afcefad20cc49d266a67343633c8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
