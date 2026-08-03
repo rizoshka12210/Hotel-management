c285d50e7173694b9b2fbbb0d2f274143fd57d01
rizoshka12210 <rizoshka12210@gmail.com>
2026-08-03 07:50:09 +0500

    update

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89a0d64c3b070893cd58251e3d89b825f3ea7c1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b4036c0a72ad17fb269fffadbf9b7cf9610ec81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
